19f0ce7dd3e0b48ddf1261d51b868ed2e68017ac
xiaohui.lam <xiaohui.lam@icloud.com>
2019-05-24 14:23:58 +0800

    enhancement

--- src/DeactivatedFluent.php
+++ src/DeactivatedFluent.php
@@ -5,12 +5,12 @@ use Illuminate\Database\Schema\Blueprint;
 
 class DeactivatedFluent
 {
-    public function addColumn(Blueprint $table)
+    public static function addColumn(Blueprint $table)
     {
         $table->timestampTz('deactivated_at')->nullable();
     }
 
-    public function dropColumn(Blueprint $table)
+    public static function dropColumn(Blueprint $table)
     {
         $table->dropColumn('deactivated_at');
     }
